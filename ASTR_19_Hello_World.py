cdcacdaafe4aacf7be277afcc073e53315c0aaca
gavinhahn <gbhahn@ucsc.edu>
2026-01-27 21:09:27 -0800

    Code Journal 1.1 update

--- ASTR_19_Hello_World.py
+++ ASTR_19_Hello_World.py
@@ -1,5 +1,12 @@
 # ASTR 19 Code Journal 1 #1
 
-print("Gavin Hahn He/Him")
-print("My favorite movie is Back To The Future.")
-print("My favorite food is chicken-pot-pie.")
+def printBio() :
+	print("Gavin Hahn He/Him")
+	print("My favorite movie is Back To The Future.")
+	print("My favorite food is chicken-pot-pie.")
+
+def main() :
+	printBio() 
+
+if __name__ == "__main__" :
+	main()
